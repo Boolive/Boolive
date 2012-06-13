def362170297827ff0e1e8a749105d7bb5be0cf5
Vova <boolive@yandex.ru>
2012-06-13 11:36:18 +0600

    Added a class for working with databases

--- www/engine/config.classes.php
+++ www/engine/config.classes.php
@@ -7,21 +7,25 @@ $classes = array(
 	'Engine\Engine'			=> 'Engine.php',
 
 	// Управление классами
-	'Engine\Classes'			=> 'classes/Classes.php',
+	'Engine\Classes'		=> 'classes/Classes.php',
 
 	// Отладка и сбор статистики по движку
-	'Engine\Trace'				=> 'develop/Trace.php',
+	'Engine\Trace'			=> 'develop/Trace.php',
 	'Engine\ITrace'			=> 'develop/Trace.php',
-	'Engine\Benchmark'			=> 'develop/Benchmark.php',
+	'Engine\Benchmark'		=> 'develop/Benchmark.php',
 
 	// Обработка ошибок
-	'Engine\ErrorsHandler'			=> 'errors/ErrorsHandler.php',
-	'Engine\Error'		=> 'errors/Error.php',
+	'Engine\ErrorsHandler'	=> 'errors/ErrorsHandler.php',
+	'Engine\Error'			=> 'errors/Error.php',
 	
 	// События
 	'Engine\Events'			=> 'events/Events.php',
-	'Engine\EventResult'		=> 'events/EventResult.php',
+	'Engine\EventResult'	=> 'events/EventResult.php',
 
 	// Поддержка Юникода (UTF-8)
-	'Engine\Unicode'			=> 'unicode/Unicode.php',
+	'Engine\Unicode'		=> 'unicode/Unicode.php',
+
+	// База данных
+	'Engine\DB'					=> 'database/DB.php',
+	'Engine\DebugPDOStatement'	=> 'database/DebugPDOStatement.php',
 );
