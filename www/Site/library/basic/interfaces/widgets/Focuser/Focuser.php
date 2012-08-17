211ce68b03c187bf8ab463c66c19fc904e616f30
Vladimir Shestakov <boolive@yandex.ru>
2012-08-17 15:17:52 +0600

    Fixed the choice of the first object of the search results

--- www/Site/library/basic/interfaces/widgets/Focuser/Focuser.php
+++ www/Site/library/basic/interfaces/widgets/Focuser/Focuser.php
@@ -14,7 +14,8 @@ use library\basic\interfaces\widgets\Widget\Widget,
 
 class Focuser extends Widget
 {
-    public function canWork(){
+    public function canWork()
+    {
         if ($result = parent::canWork()){
             // По URL определяем объект и номер страницы
             $uri = $this->_input->GET->path->string();
@@ -27,7 +28,7 @@ class Focuser extends Widget
             $object = null;
             // объект по умолчанию
             if (empty($uri) && ($object = Data::object('/contents')->find(array('count'=>1, 'order'=>'`order` ASC')))){
-                $object = $object[0];
+                $object = reset($object);
             }
             // ищем в /contents
             if (!$object && !empty($uri)) $object = Data::object('/contents/'.$uri);
@@ -40,9 +41,4 @@ class Focuser extends Widget
         }
         return $result;
     }
-
-//    public function work($v = array()){
-//        trace($this->_input);
-//        return parent::work($v);
-//    }
 }
