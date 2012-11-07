848146faad00541b6a464d62240929f8c773b2e9
Vladimir Shestakov <boolive@yandex.ru>
2012-11-07 16:18:38 +0600

    Added class Auth to get current user. Class is not complete.

---
+++ www/Boolive/auth/Auth.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Модуль авторизации и аутентификации пользователя
+ * Определяет текущего пользователя, выполняет вход/выход и регистрацию пользователя
+ *
+ * @version 1.0
+ * @author Vladimir Shestakov <boolive@yandex.ru>
+ */
+namespace Boolive\auth;
+
+class Auth
+{
+    /**
+     * Текущий пользователь
+     * @return \Boolive\data\Entity
+     */
+    static function getUser()
+    {
+        return \Boolive\data\Data::getObject('/Members/registered/admins/vova');
+    }
+}
