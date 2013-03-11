e60927570e43a5ebb106df4d8be82bfff91a618a
Vladimir Shestakov <boolive@yandex.ru>
2013-03-11 18:24:59 +0600

    fixed style of the AuthMenu

--- www/Library/layouts/Admin/MenuAuth/MenuAuth.tpl
+++ www/Library/layouts/Admin/MenuAuth/MenuAuth.tpl
@@ -1,15 +1,15 @@
 <div class="MenuAuth" data-v="<?=$v['view_uri']?>" data-p="MenuAuth">
     <ul class="buttons">
         <?php if (isset($v['name'])): ?>
-        <li>
-            <a title="Профиль пользователя" class="userlink" data-o="<?=$v['userlink']?>" href="/admin<?php echo $v['userlink'];?>"><?php echo $v['name'];?></a>
+        <li class="userlink">
+            <a title="Профиль пользователя" data-o="<?=$v['userlink']?>" href="/admin<?php echo $v['userlink'];?>"><?php echo $v['name'];?></a>
         </li>
-        <li>
-            <a title="Отмена авторизации" class="exit" href="<?php echo $v['logout'];?>">Выйти</a>
+        <li class="exit">
+            <a title="Отмена авторизации" href="<?php echo $v['logout'];?>">Выйти</a>
         </li>
         <?php endif;?>
-        <li>
-            <a title="Главная страница сайта" class="site" href="/">На сайт</a>
+        <li class="site">
+            <a title="Главная страница сайта" href="/">На сайт</a>
         </li>
 
     </ul>
--- www/Library/layouts/Admin/MenuAuth/res/jquery.ui.MenuAuth/jquery.ui.MenuAuth.js
+++ www/Library/layouts/Admin/MenuAuth/res/jquery.ui.MenuAuth/jquery.ui.MenuAuth.js
@@ -8,10 +8,10 @@
         _create: function() {
             $.boolive.AjaxWidget.prototype._create.call(this);
             var self = this;
-            this.element.find('.userlink').click(function(e){
+            this.element.find('.userlink a').click(function(e){
                 e.preventDefault();
                 self.callParents('setState', [{
-                    object: self.element.find('.userlink').attr('data-o')
+                    object: $(this).attr('data-o')
                 }]);
             });
             this.call_setState({target: this, direct: 'children'}, this.callParents('getState'), {object: true});
@@ -25,7 +25,7 @@
         call_setState: function(caller, state, changes){
             if ($.isPlainObject(changes) && ('object' in changes)){
                 var uri = state.object;
-                var link = this.element.find('.userlink');
+                var link = this.element.find('.userlink a');
                 if(uri == link.attr('data-o')) {
                     link.parent().addClass('active');
                 }else{
