dd3fc6930be00d2219f249689cf9121dc0cbe296
Vladimir Shestakov <boolive@yandex.ru>
2013-07-27 22:57:07 +0600

    Updatings for object are automatically checked when viewing his in the admin panel

--- www/Library/admin_widgets/Programs/Programs.php
+++ www/Library/admin_widgets/Programs/Programs.php
@@ -6,6 +6,8 @@
  */
 namespace Library\admin_widgets\Programs;
 
+use Boolive\data\Data;
+use Boolive\data\Entity;
 use Library\views\AutoWidget2\AutoWidget2,
     Boolive\values\Rule;
 
@@ -23,5 +25,18 @@ class Programs extends AutoWidget2
                 )
             )
         );
+    }
+
+    public function work($v = array())
+    {
+        // Проверка обновлений для объекта
+        if ($this->_input['REQUEST']['object'] instanceof Entity){
+            /** @var Entity $obj */
+            $obj = $this->_input['REQUEST']['object'];
+            if ($obj->_attribs['update_time']!=0 && ($obj->_attribs['update_step']!=0 || (time()-$obj->_attribs['update_time']) > 300)){
+                Data::refresh($obj, 50, 1, true);
+            }
+        }
+        return parent::work($v);
     }
 }
