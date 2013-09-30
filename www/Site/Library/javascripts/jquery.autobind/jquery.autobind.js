a8b1db9ca58a5196b1fc70b819bb4dc83acb1b08
Vladimir Shestakov <boolive@yandex.ru>
2013-09-30 20:28:02 +0600

    Use attribute data-p-options for jQuery plugins options (json)

--- www/Site/Library/javascripts/jquery.autobind/jquery.autobind.js
+++ www/Site/Library/javascripts/jquery.autobind/jquery.autobind.js
@@ -7,8 +7,10 @@
     var autobind = function(context){
         context.find('[data-p]').each(function(){
             var plugin = $(this).attr('data-p');
+            var options = $(this).data('p-option');
+            console.log(options);
             if (typeof $(this)[plugin] == 'function'){
-                $(this)[plugin]().removeAttr('data-p');
+                $(this)[plugin](options).removeAttr('data-p');
             }
         });
     };
