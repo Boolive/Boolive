9ea67793735ae981a3ac773a35dbbfaa38fb83c5
Ishodnikov <gpavellog@gmail.com>
2013-10-01 22:48:17 +0400

    test commit

--- www/Site/Library/admin_widgets/HTMLEditor/res/Ckeditor/config.js
+++ www/Site/Library/admin_widgets/HTMLEditor/res/Ckeditor/config.js
@@ -8,7 +8,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
         { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
         { name: 'undo'},
