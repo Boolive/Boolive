ff3b89658d364b595c67717cc8c3ba0676ce88b3
Vladimir Shestakov <boolive@yandex.ru>
2013-12-07 16:07:19 +0600

    Fixes in HTMLBlockPreview

--- www/Site/Library/content_widgets/PagePreview/views/HTMLBlockPreview/HTMLBlockPreview.php
+++ www/Site/Library/content_widgets/PagePreview/views/HTMLBlockPreview/HTMLBlockPreview.php
@@ -11,60 +11,59 @@ use Library\views\HtmlBlock\HtmlBlock;
 
 class HTMLBlockPreview extends HtmlBlock
 {
-
     function show($v = array(), $commands, $input)
     {
         $v['object'] = $this->_input['REQUEST']['object']->value();
-
         $text = $v['object'];
         $len = mb_strlen($text);
-
-        $tags = array(); // массив, в который записываем кол-во открытых тэгов
-
-        $r = preg_split('/(<[^>]*>)/im',$text,-1,PREG_SPLIT_DELIM_CAPTURE);
-
-        $c =0 ; // счетчик длины строки
-        $mx = (mb_strpos($text,'<a class="more"> </a>') ? mb_strpos($text,'<a class="more"> </a>') : $len);; // макс. длина текста
-        $cw = true; // достигли ли макс. - можно заменить на ($mx>$c)
-        $res = ''; // результат
-
+        // массив, в который записываем кол-во открытых тэгов
+        $tags = array();
+        $r = preg_split('/(<[^>]*>)/im', $text, -1, PREG_SPLIT_DELIM_CAPTURE);
+        // счетчик длины строки
+        $c = 0;
+        // макс. длина текста
+        $mx = (mb_strpos($text,'<a class="more"> </a>') ? mb_strpos($text,'<a class="more"> </a>') : $len);
+        // достигли ли макс. - можно заменить на ($mx>$c)
+        $cw = true;
+        // результат
+        $res = '';
         foreach ($r as $w) {
-
             // если тэг
             if (preg_match('/<(\/?)([a-z]+)\s*[^>]*>/ims',$w,$m)) {
-                if ($m[1]!='/') { // открывающий
-
-                    if ($cw){ // если еще не макс., то пишем, добавляем в счетчик тэгов
-                        $tags[mb_strtolower($m[2])]++;
-                        $res.=$w;
+                $key = mb_strtolower($m[2]);
+                if (!isset($tags[$key])) $tags[$key] = 0;
+                if ($m[1] != '/'){
+                    // открывающий
+                    if ($cw){
+                        // если еще не макс., то пишем, добавляем в счетчик тэгов
+                        $tags[$key]++;
+                        $res.= $w;
                     }
-                } else { // закрывыющий тэг
-
+                }else{
+                    // закрывыющий тэг
                     // уменьшаем счетчик , если есть такой открытый тэг - пишем
-                    if ($tags[mb_strtolower($m[2])]>0) {
-                        $res.=$w;
+                    if ($tags[$key] > 0) {
+                        $res.= $w;
                     }
-                    $tags[mb_strtolower($m[2])]--;
+                    $tags[$key]--;
                 }
             }
-            if ($cw) {
-                $len = mb_strlen($w); // длина текста
-                if ($len+$c>$mx) { // достигли?
+            if ($cw){
+                // длина текста
+                $len = mb_strlen($w);
+                // достигли?
+                if ($len+$c>$mx){
                     // обрезаем, добавляем ...
                     $res.=mb_substr($w,0,$mx-$c);
                     $cw = false;
-                }else {
-
+                }else{
                     $res.=$w;
                     $c+=$len;
                 }
             }
         }
-
         $output = $res;
-
         $v['object'] = $output;
-
         return Widget::show($v, $commands, $input);
     }
 }
