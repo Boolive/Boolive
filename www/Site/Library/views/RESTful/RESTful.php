00dadf99f88b0cb835f877510aefcd22fe639670
Vladimir Shestakov <boolive@yandex.ru>
2013-12-12 01:55:46 +0600

    Improvings in Restful. Normalization of input data. Exception handling before saving object

--- www/Site/Library/views/RESTful/RESTful.php
+++ www/Site/Library/views/RESTful/RESTful.php
@@ -55,8 +55,7 @@ class RESTful extends View
             case 'POST':
             // Редактирование или создание объекта, если его нет
             case 'PUT':
-
-                $attribs = $this->_input['REQUEST']['entity'];
+                $attribs = $this->normolizeAttribs($this->_input['REQUEST']['entity']);
                 if (isset($this->_input['FILES']['entity']['file'])){
                     $attribs['file'] = $this->_input['FILES']['entity']['file'];
                 }else{
@@ -134,37 +133,38 @@ class RESTful extends View
      */
     private function put($obj, $attribs)
     {
-        // Изменяемый объект
-        $create = !$obj->isExist();
-        // Название
-        if (isset($attribs['name']) && ($attribs['name']!=$obj->name() || !empty($attribs['autoname']))) $obj->name($attribs['name'], true);
-        // Значение
-        if (empty($attribs['is_default_value'])){
-            if (isset($attribs['value'])) $obj->value($attribs['value']);
-        }else{
-            // Значения по умолчанию от прототипа
-            $obj->isDefaultValue(true);
-        }
-        // Файл
-        if (isset($attribs['file'])){
-            $obj->file($attribs['file']);
-        }else{
-            if (isset($attribs['is_file'])) $obj->isFile(!empty($attribs['is_file']));
-        }
-        if (isset($attribs['proto'])) $obj->proto(Data::read($attribs['proto']));
-        if (isset($attribs['parent'])) $obj->parent(Data::read($attribs['parent']));
-        if (isset($attribs['order'])) $obj->order($attribs['order']);
-        if (isset($attribs['is_hidden'])) $obj->isHidden(!empty($attribs['is_hidden']));
-        if (isset($attribs['is_draft'])) $obj->isDraft(!empty($attribs['is_draft']));
-        if (isset($attribs['is_link'])) $obj->isLink(!empty($attribs['is_link']));
-        if (isset($attribs['is_relative'])) $obj->isRelative(!empty($attribs['is_relative']));
-        if (isset($attribs['is_mandatory'])) $obj->isMandatory($attribs['is_mandatory']);
-        $class_changed = isset($attribs['is_default_class']) && (bool)$obj->isDefaultClass() != empty($attribs['is_default_class']);
-        if (isset($attribs['is_default_class'])){
-            $obj->isDefaultClass(!empty($attribs['is_default_class']));
-        }
-        // Проверка и сохранение
         try{
+            // Изменяемый объект
+            $create = !$obj->isExist();
+            // Название
+            if (isset($attribs['name']) && ($attribs['name']!=$obj->name() || !empty($attribs['autoname']))) $obj->name($attribs['name'], true);
+            // Значение
+            if (empty($attribs['is_default_value'])){
+                if (isset($attribs['value'])) $obj->value($attribs['value']);
+            }else{
+                // Значения по умолчанию от прототипа
+                $obj->isDefaultValue(true);
+            }
+            // Файл
+            if (isset($attribs['file'])){
+                $obj->file($attribs['file']);
+            }else{
+                if (isset($attribs['is_file'])) $obj->isFile(!empty($attribs['is_file']));
+            }
+            if (isset($attribs['proto'])) $obj->proto(Data::read($attribs['proto']));
+            if (isset($attribs['parent'])) $obj->parent(Data::read($attribs['parent']));
+            if (isset($attribs['order'])) $obj->order($attribs['order']);
+            if (isset($attribs['is_hidden'])) $obj->isHidden(!empty($attribs['is_hidden']));
+            if (isset($attribs['is_draft'])) $obj->isDraft(!empty($attribs['is_draft']));
+            if (isset($attribs['is_link'])) $obj->isLink(!empty($attribs['is_link']));
+            if (isset($attribs['is_relative'])) $obj->isRelative(!empty($attribs['is_relative']));
+            if (isset($attribs['is_mandatory'])) $obj->isMandatory(!empty($attribs['is_mandatory']));
+            $class_changed = isset($attribs['is_default_class']) && (bool)$obj->isDefaultClass() != empty($attribs['is_default_class']);
+            if (isset($attribs['is_default_class'])){
+                $obj->isDefaultClass(!empty($attribs['is_default_class']));
+            }
+            // Проверка и сохранение
+
             $obj->save(false);
             // Если изменился класс, то повторно выбрать объект из хранилища, чтобы обновилась его логика
             if ($class_changed){
@@ -190,36 +190,38 @@ class RESTful extends View
      */
     private function post($parent, $attribs)
     {
-        if (isset($attribs['proto']) && ($proto = Data::read($attribs['proto'])) && $proto->isExist()){
-            $obj = $proto->birth($parent, false);
-        }else{
-            $obj = new Entity();
-            $obj->parent($parent);
-        }
-        // Название
-        if (isset($attribs['name'])) $obj->name($attribs['name'], true);
-        // Значение
-        if (empty($attribs['is_default_value'])){
-            if (isset($attribs['value'])) $obj->value($attribs['value']);
-        }else{
-            // Значения по умолчанию от прототипа
-            $obj->isDefaultValue(true);
-        }
-        // Файл
-        if (isset($attribs['file'])){
-            $obj->file($attribs['file']);
-        }else{
-            $obj->isFile(!empty($attribs['is_file']));
-        }
-        if (isset($attribs['order'])) $obj->order($attribs['order']);
-        if (isset($attribs['is_hidden'])) $obj->isHidden(!empty($attribs['is_hidden']));
-        if (isset($attribs['is_draft'])) $obj->isDraft(!empty($attribs['is_draft']));
-        if (isset($attribs['is_link'])) $obj->isLink(!empty($attribs['is_link']));
-        if (isset($attribs['is_default_class'])){
-            $obj->isDefaultClass(!empty($attribs['is_default_class']));
-        }
-        // Проверка и сохранение
         try{
+            if (isset($attribs['proto']) && ($proto = Data::read($attribs['proto'])) && $proto->isExist()){
+                $obj = $proto->birth($parent, false);
+            }else{
+                $obj = new Entity();
+                $obj->parent($parent);
+            }
+            // Название
+            if (isset($attribs['name'])) $obj->name($attribs['name'], true);
+            // Значение
+            if (empty($attribs['is_default_value'])){
+                if (isset($attribs['value'])) $obj->value($attribs['value']);
+            }else{
+                // Значения по умолчанию от прототипа
+                $obj->isDefaultValue(true);
+            }
+            // Файл
+            if (isset($attribs['file'])){
+                $obj->file($attribs['file']);
+            }else{
+                $obj->isFile(!empty($attribs['is_file']));
+            }
+            if (isset($attribs['order'])) $obj->order($attribs['order']);
+            if (isset($attribs['is_hidden'])) $obj->isHidden(!empty($attribs['is_hidden']));
+            if (isset($attribs['is_draft'])) $obj->isDraft(!empty($attribs['is_draft']));
+            if (isset($attribs['is_link'])) $obj->isLink(!empty($attribs['is_link']));
+            if (isset($attribs['is_relative'])) $obj->isRelative(!empty($attribs['is_relative']));
+            if (isset($attribs['is_mandatory'])) $obj->isMandatory(!empty($attribs['is_mandatory']));
+            if (isset($attribs['is_default_class'])){
+                $obj->isDefaultClass(!empty($attribs['is_default_class']));
+            }
+            // Проверка и сохранение
             $obj->save(false);
             header("HTTP/1.1 201 Created");
             header('Content-Type: application/json; charset=UTF-8');
@@ -247,5 +249,21 @@ class RESTful extends View
         }
         header('Content-Type: application/json; charset=UTF-8');
         echo F::toJSON(array('result'=>$result));
+    }
+
+    private function normolizeAttribs($attribs)
+    {
+        foreach ($attribs as $key => $value){
+            if ($value === 'false'){
+                $attribs[$key] = false;
+            }else
+            if ($value === 'true'){
+                $attribs[$key] = true;
+            }else
+            if ($value === 'null'){
+                $attribs[$key] = null;
+            }
+        }
+        return $attribs;
     }
 }
