d0b46f0a180a51ace99a81aeed1dd08fba12cb43
Vladimir Shestakov <boolive@yandex.ru>
2013-12-17 02:04:08 +0600

    Fixes in jquery.include - loading of *.js files is made the synchronous

--- www/Site/Library/javascripts/jquery.include/jquery.include.js
+++ www/Site/Library/javascripts/jquery.include/jquery.include.js
@@ -28,79 +28,62 @@
 	$.extend({
 		// You can change the base path to be applied in all imports
 		ImportBasePath: '',
-		// Associative array storing wating tasks and their callback
-		__WaitingTasks: new Object(),
-		// Called when a single file is loaded successfully - update and check WaitingTasks to see if it's ok to load callback
-		__loadedSuccessfully: function(taskId){
-			if (taskId in $.__WaitingTasks){
-				if (($.__WaitingTasks[taskId].loading -= 1) < 1){
-					var callback = $.__WaitingTasks[taskId].task;
-					if (typeof callback == 'function') {
-						callback();
-					}
-					delete $.__WaitingTasks[taskId];
-				}
-			}
-		},
-		//pass a file name and return a array with file name and extension
-		fileinfo:	function(data){
+	    //pass a file name and return a array with file name and extension
+		fileinfo: function(data){
             data = data.replace(/^\s|\s$/g, "");
 			var m;
-            if (/\.[\w\?]+$/.test(data)) {
+            if (/\.[\w\?]+$/.test(data)){
                 m = data.match(/([^\/\\]+)\.(\w+)(\?.+)?$/);
-                if (m) {
-					if (m[2] == 'js') {
+                if (m){
+					if (m[2] === 'js'){
 						return {
 							filename: m[1],
 							ext: m[2],
 							tag: 'script'
 						};
-					}
-					else 
-						if (m[2] == 'css') {
+					}else{
+						if (m[2] === 'css'){
 							return {
 								filename: m[1],
 								ext: m[2],
 								tag: 'link'
 							};
-						}
-						else {
+						}else{
 							return {
 								filename: m[1],
 								ext: m[2],
 								tag: null
 							};
 						}
-				}
-				else {
+                    }
+				}else{
 					return {
 						filename: null,
 						ext: null
 					};
 				}
-            } else {
+            }else{
                 m = data.match(/([^\/\\]+)$/);
-                if (m) {
+                if (m){
 					return {
 						filename: m[1],
 						ext: null,
 						tag: null
 					};
-				}
-				else {
+				}else{
 					return {
 						filename: null,
 						ext: null,
 						tag: null
 					};
-				}	
+				}
             }
         },
 		//Check if the file that is been included already exist and return a Boolean value
-		fileExist: function(filename,filetype,attrCheck) {
+		fileExist: function(filename, filetype, attrCheck) {
 			var elementsArray = document.getElementsByTagName(filetype);
-			for(var i=0;i<elementsArray.length;i++) {
-				if(elementsArray[i].getAttribute(attrCheck)==$.ImportBasePath+filename) {
+			for(var i = 0; i < elementsArray.length; i++) {
+				if(elementsArray[i].getAttribute(attrCheck) === $.ImportBasePath + filename) {
 					return true;
 				}
 			}
@@ -110,95 +93,109 @@
 		createElement: function(filename,filetype) {
 			switch(filetype) {
 				case 'script' :
-				if (!$.fileExist(filename, filetype, 'src')) {
-					var scriptTag = document.createElement(filetype);
-					scriptTag.setAttribute('language', 'javascript');
-					scriptTag.setAttribute('type', 'text/javascript');
-					scriptTag.setAttribute('src', $.ImportBasePath + filename);
-					return scriptTag;
-				} else {
-					return false;
-				}
-				break;
+                    if (!$.fileExist(filename, filetype, 'src')) {
+                        var scriptTag = document.createElement(filetype);
+                        scriptTag.setAttribute('language', 'javascript');
+                        scriptTag.setAttribute('type', 'text/javascript');
+                        scriptTag.setAttribute('src', $.ImportBasePath + filename);
+                        return scriptTag;
+                    }else{
+                        return false;
+                    }
+				    break;
 				case 'link' :
-				if (!$.fileExist(filename, filetype, 'href')) {
-					var styleTag = document.createElement(filetype);
-					styleTag.setAttribute('type', 'text/css');
-					styleTag.setAttribute('rel', 'stylesheet');
-					styleTag.setAttribute('href', $.ImportBasePath + filename);
-					return styleTag;
-				} else {
-					return false;
-				}
-				break;
-
+                    if (!$.fileExist(filename, filetype, 'href')) {
+                        var styleTag = document.createElement(filetype);
+                        styleTag.setAttribute('type', 'text/css');
+                        styleTag.setAttribute('rel', 'stylesheet');
+                        styleTag.setAttribute('href', $.ImportBasePath + filename);
+                        return styleTag;
+                    }else{
+                        return false;
+                    }
+                    break;
 				default :
 					return false;
-				break;
 			}
-		},
-		cssReady: function(index, taskId) {
-			function check() {
-				if(document.styleSheets[index]){
-					window.clearInterval(checkInterval);
-					$.__loadedSuccessfully(taskId);
-				}
-			}
-			var checkInterval = window.setInterval(check,200);
 		},
 		//The main function to insert the file
-		include: function(file,callback) {
+		include: function(file, callback) {
 			var headerTag = document.getElementsByTagName('head')[0];
 			var fileArray = [];
+            var elements = [];
 			//if file is string, give a single index element
 			typeof file=='string' ? fileArray[0] = file : fileArray = file;
-			// Create a unique id using the current time
-			var taskId = new Date().getTime().toString();
-			$.__WaitingTasks[taskId] = {'loading': fileArray.length, 'task': callback};
 			//go through all the files
 			var length = fileArray.length;
-			if (length == 0) $.__loadedSuccessfully(taskId);
-			for (var i = 0; i < length; i++) {
+			for (var i = 0; i < length; i++){
 				var elementTag = $.fileinfo(fileArray[i]).tag;
-				var el = [];
+				var el;
 				if (elementTag !== null) {
-					var css_index = document.getElementsByTagName(elementTag).length;
-					el[i] = $.createElement(fileArray[i], elementTag);
-					if (el[i]) {
-						headerTag.appendChild(el[i]);
-						//if ($.browser.msie) { //msie
-						if (/(msie) ([\w.]+)/i.test(navigator.userAgent)) {
-								el[i].onreadystatechange = function(){
-								if (this.readyState === 'loaded' || this.readyState === 'complete') {
-									$.__loadedSuccessfully(taskId);
-								}
-							};
-						}
-						else {
-							if (elementTag == 'link') {
-                                $.__loadedSuccessfully(taskId);
-								//$.cssReady(css_index, taskId);
-							}
-							else {
-                                if (/WebKit/i.test(navigator.userAgent)) {
-                                    var _timer = setInterval(function(){
-                                        if (/loaded|complete/.test(document.readyState)) {
-                                            $.__loadedSuccessfully(taskId); // call of the call
-                                        }
-                                    }, 100);
-                                }
-								el[i].onload = function(){
-									$.__loadedSuccessfully(taskId);
-								};
-							}
-						}
-					}else{
-						$.__loadedSuccessfully(taskId);
-					}
-				} else {
-					i = length; //stop for
-				}
-			}
+					el = $.createElement(fileArray[i], elementTag);
+					if (el) {
+						if (elementTag === 'link'){
+                            headerTag.appendChild(el);
+                        }else{
+                            elements.push(el);
+                        }
+                    }
+                }
+            }
+            var append = function(elements, success){
+                if (!elements.length){
+                    success();
+                }else{
+                    var el = elements.shift();
+                    headerTag.appendChild(el);
+                    if (/(msie) ([\w.]+)/i.test(navigator.userAgent)){
+                        el.onreadystatechange = function(){
+                            if (this.readyState === 'loaded' || this.readyState === 'complete') {
+                                append(elements, callback);
+                            }
+                        };
+                    }else{
+                        el.onload = function(){
+                            append(elements, callback);
+                        };
+                    }
+                }
+            };
+            append(elements, callback);
+
+//                        headerTag.appendChild(el[i]);
+//						//if ($.browser.msie) { //msie
+//						if (/(msie) ([\w.]+)/i.test(navigator.userAgent)){
+//								el[i].onreadystatechange = function(){
+//								if (this.readyState === 'loaded' || this.readyState === 'complete') {
+//									$.__loadedSuccessfully(taskId);
+//								}
+//							};
+//						}else{
+//							if (elementTag === 'link'){
+//                                $.__loadedSuccessfully(taskId);
+//								//$.cssReady(css_index, taskId);
+//							}else{
+////                                if (/WebKit/i.test(navigator.userAgent)) {
+////                                    var check = function(){
+////                                        if (/loaded|complete/.test(document.readyState)) {
+////                                            $.__loadedSuccessfully(taskId); // call of the call
+////                                            window.clearInterval(_timer);
+////                                        }
+////                                    };
+////                                    var _timer = window.setInterval(check, 100);
+////                                }
+//								el[i].onload = function(){
+//									$.__loadedSuccessfully(taskId);
+//								};
+//							}
+//						}
+//					}else{
+//						$.__loadedSuccessfully(taskId);
+//					}
+//				}else{
+//					i = length; //stop for
+//				}
+//			}
 		}
 	});
 })(jQuery);
