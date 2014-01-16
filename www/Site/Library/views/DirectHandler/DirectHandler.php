96b9813287faa8a5886713f4ddc7fbe2a63dc893
Vladimir Shestakov <boolive@yandex.ru>
2014-01-17 01:36:51 +0600

    microfix of the DirectHandler

--- www/Site/Library/views/DirectHandler/DirectHandler.php
+++ www/Site/Library/views/DirectHandler/DirectHandler.php
@@ -27,7 +27,7 @@ class DirectHandler extends View
     function work()
     {
         $v = array();
-        $v['out'] = $this->_input['REQUEST']['direct']->start($this->_commands, $this->_input_child);
+        $v['out'] = $this->_input['REQUEST']['direct']->linked()->start($this->_commands, $this->_input_child);
 		// Вместе с html нужно передать пути на css и js
         foreach ($this->_commands->get('htmlHead') as $com){
             if (in_array($com[0], array("link", "script"/*, "meta", "title", "style"*/))){
