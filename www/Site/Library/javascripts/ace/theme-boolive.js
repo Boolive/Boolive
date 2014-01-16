678193dd7ed13dc65f5bb42850f05041c0388e9d
Vladimir Shestakov <boolive@yandex.ru>
2014-01-17 01:39:03 +0600

    fixes in the theme of ace editor

--- www/Site/Library/javascripts/ace/theme-boolive.js
+++ www/Site/Library/javascripts/ace/theme-boolive.js
@@ -4,7 +4,7 @@ ace.define("ace/theme/boolive", ["require", "exports", "module", "ace/lib/dom"],
 
 
 
-        '.ace-boolive .ace_gutter {background: #F7F7F7; color: #AAA; border-right: 1px dotted #ccc;}' +
+        '.ace-boolive .ace_gutter {background: transparent; color: #ccc; border-right: 1px dotted #ccc;}' +
         '.ace-boolive  {background: #fff;color: #000;}' +
         '.ace-boolive .ace_tag {color:#0000FF;}' +
         '.ace-boolive .ace_attribute-name {color:#800000;}' +
@@ -31,7 +31,7 @@ ace.define("ace/theme/boolive", ["require", "exports", "module", "ace/lib/dom"],
 //        '.ace-boolive .ace_variable.ace_instance {color: teal;}' +
         '.ace-boolive .ace_constant.ace_language {color: #252FFF;}' +
         '.ace-boolive .ace_cursor {color: black;}' +
-        '.ace-boolive .ace_marker-layer .ace_active-line {background: #FFFFCC;}' +
+        '.ace-boolive .ace_marker-layer .ace_active-line {background: transparnt;/*#FFFFCC;*/}' +
         '.ace-boolive .ace_marker-layer .ace_selection {background: #B0C5E3;}' +
         '/* bold keywords cause cursor issues for some fonts */' +
         '/* this disables bold style for editor and keeps for static highlighter */' +
@@ -39,7 +39,7 @@ ace.define("ace/theme/boolive", ["require", "exports", "module", "ace/lib/dom"],
         '.ace-boolive .ace_marker-layer .ace_step {background: rgb(252, 255, 0);}' +
         '.ace-boolive .ace_marker-layer .ace_stack {background: rgb(164, 229, 101);}' +
         '.ace-boolive .ace_marker-layer .ace_bracket {margin: 0;border: 0; background: #9AD7FF;}' +
-        '.ace-boolive .ace_gutter-active-line {background-color : rgba(255,255,204,0.5);}' +
+        '.ace-boolive .ace_gutter-active-line {background-color: transparent;}' +
         '.ace-boolive .ace_marker-layer .ace_selected-word {background: rgb(250, 250, 255);border: 1px solid rgb(200, 200, 250);}' +
         '.ace-boolive .ace_print-margin {width: 1px;background: #e8e8e8;}' +
         '.ace-boolive .ace_indent-guide {background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==") right repeat-y;}';
