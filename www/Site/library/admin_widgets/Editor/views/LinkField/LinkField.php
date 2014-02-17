55c9bed0e5b2fa0ac9f04ecea5137d49a0c26cd0
Vladimir Shestakov <boolive@yandex.ru>
2014-02-18 00:56:53 +0600

    Added LinkField (not completed)

---
+++ www/Site/library/admin_widgets/Editor/views/LinkField/LinkField.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Поле объекта по ссылке
+ * 
+ * @version 1.0
+ */
+namespace Site\library\admin_widgets\Editor\views\LinkField;
+
+use Site\library\admin_widgets\BaseExplorer\views\Item\Item;
+
+class LinkField extends Item
+{
+    function show($v = array(), $commands, $input)
+    {
+        return parent::show($v,$commands, $input);
+    }
+}
